436b536821fbbd02a79beedda6e73ebe15efa656
elemeNtk0 <elemeNtk0gm@gmail.com>
2021-06-09 18:18:15 +0300

    finish 16 lesson

--- udemy_js/js_react/lesson_16/js/script.js
+++ udemy_js/js_react/lesson_16/js/script.js
@@ -1,7 +1,35 @@
 'use strict';
 
+let num = 20;
+
 function showFirstMessage(text) {
   console.log(text);
+  let num = 10;
+  console.log(num);
 }
 
 showFirstMessage('Hello Yopta!');
+console.log(num);
+
+// function существует до объявления. Т.е. консоль лог можно вызвать перед функцией
+console.log(calc(10, 6));
+function calc(a, b) {
+  return a + b;
+}
+
+console.log(calc(4, 3));
+console.log(calc(5, 6));
+
+function ret() {
+  let num = 50;
+  return num;
+}
+
+const anotherNum = ret();
+console.log(anotherNum);
+
+const logger = function () {
+  console.log('Hello');
+};
+
+logger();
